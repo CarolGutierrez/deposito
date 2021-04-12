1217752a684594404059f417e55d39d33604450a
CarolGutierrez <neylin_gutierrez_@hotmail.com>
2021-04-13 00:06:38 +0200

    getDepositoMax

---
+++ src/getDepositoMaxTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class getDepositoMaxTest {
+
+	@Test
+	void testGetDepositoMax() {
+		fail("Not yet implemented");
+	}
+
+}
