64989dce865c284f90beefa9a622742541dd5840
CarolGutierrez <neylin_gutierrez_@hotmail.com>
2021-04-13 00:23:59 +0200

    fill 2

--- src/fillTest.java
+++ src/fillTest.java
@@ -1,12 +1,24 @@
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class fillTest {
-
+DepositoCombustible tank;
+	
+	@BeforeEach
+		public void before() {
+		tank = new DepositoCombustible(40.00, 0.00);
+	}
+		
 	@Test
-	void testFill() {
-		fail("Not yet implemented");
+	public void testFill() {
+		tank.fill(10.00);
+	}
+	
+	@AfterEach
+	public void after() {
+		System.out.println("Nivel combustible: " + tank.getDepositoNivel());
 	}
 
 }
